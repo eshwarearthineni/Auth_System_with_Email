e1ad2edfab5f8ec88da3a8f57187909acdbbc534
ESHWAR EARTHINENI <eshwarearthineni@gmail.com>
2025-02-10 16:23:02 +0530

    Here commiting backend

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,8 +22,31 @@ export const register = async(req, res){
 
         const token = jwt.sign({id: user._id} , process.env.JWT_SECRET, {expiresIn:'7d'});
 
+        res.cookie('token',token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'producton' ?
+            'none':'strict',
+            maxAge: 7*24*60*60*1000
+
+        })
+
     }
     catch(error){
         res.json({success: false, message: error.message})
     }
+}
+
+export const login = async (req, res) =>{
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        return res.json({status: false, message: "Email and password are required"})
+    }
+    try{
+
+    }catch(error){
+        return res.json({success: false, message: error.message})
+    }
+
 }
